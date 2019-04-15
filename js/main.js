112c25f2d4a90dbb26acd227ce90917b38ce7da5
Mario Freire <huege.design@gmail.com>
2019-04-15 14:23:13 -0300

    feat: adds responsiveness to the main page

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibWpmbmV0byIsImEiOiJjanVkMTl4dXQwYzh4M3pwOHB5cjB5ZDJmIn0.1clvD37JU4YDLjRdOxdTsw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -157,28 +157,17 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  li.className = 'info-container';
+  li.setAttribute('style', `background-image: url(${DBHelper.imageUrlForRestaurant(restaurant)});`);
 
-  const image = document.createElement('img');
-  image.className = 'restaurant-img';
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
-
-  const name = document.createElement('h1');
-  name.innerHTML = restaurant.name;
-  li.append(name);
-
-  const neighborhood = document.createElement('p');
-  neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
-
-  const address = document.createElement('p');
-  address.innerHTML = restaurant.address;
-  li.append(address);
-
-  const more = document.createElement('a');
-  more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  li.innerHTML = `
+    <div class='info-wrapper'>
+      <h1><span>${restaurant.name}</span></h1>
+      <p><span>${restaurant.neighborhood}</span></p>
+      <p><span>${restaurant.address}</span></p>
+      <button class='more-button' onclick="location.href='${DBHelper.urlForRestaurant(restaurant)}'" type="button">View Details</button>
+    </div>
+  `;
 
   return li
 }
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoibWpmbmV0byIsImEiOiJjanVkMTl4dXQwYzh4M3pwOHB5cjB5ZDJmIn0.1clvD37JU4YDLjRdOxdTsw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
